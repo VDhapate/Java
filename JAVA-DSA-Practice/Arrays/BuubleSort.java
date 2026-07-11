84210be50165ce69c2e26b5f81a8e112f28363bf
Vaibhavi Dhapate <124279371+VDhapate@users.noreply.github.com>
2026-07-11 23:07:02 +0530

    Fix bubble sort implementation and naming

--- JAVA-DSA-Practice/Arrays/BuubleSort.java
+++ JAVA-DSA-Practice/Arrays/BuubleSort.java
@@ -7,13 +7,13 @@ class BuubleSort{
 		System.out.println(Arrays.toString(a));
 	}
 	public static void bubbleSort(int [] a){
-		for(int i =0; i<a.length-1; i++)
+		for(int i =0; i<a.length; i++)
 		{
-			for(int j =i+1; j<a.length; j++){
-				if(a[i]> a[j]){
-					int temp = a[i];
-					a[i] = a[j];
-					a[j] = temp;
+			for(int j =0; j<a.length-1; j++){
+				if(a[j]> a[j+1]){
+					int temp = a[j];
+					a[j] = a[j+1];
+					a[j+1] = temp;
 				}
 			}
 		}
